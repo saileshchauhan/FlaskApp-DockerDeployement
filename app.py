23c6fd124d128a64b3759b088610a02af0f1f61a
saileshchauhan <chauhansailesh68@gmail.com>
2021-09-01 11:41:23 +0530

    [sailesh] add: app.py flask file

---
+++ app.py
@@ -0,0 +1,25 @@
+from logging import info
+from flask import Flask,request,render_template
+import pickle
+
+app=Flask(__name__)
+
+@app.route('/')
+def get_user_login_details():
+  return render_template("login.html")
+database={'nachi':'123','anderson':'456','samuel':'789'}   
+
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+    user_name=request.form['username']
+    pswd=request.form['password']
+    if user_name not in database:
+        return render_template('login.html',info='Invalid user name')
+    if database[user_name]==pswd:
+        return render_template('home.html',name=user_name)
+    else:
+        return render_template('login.html',info='Invalid Password')
+
+if __name__ == "__main__":
+    app.run()
+        
